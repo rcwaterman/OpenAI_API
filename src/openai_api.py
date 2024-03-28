fcd14a1d590f03a4eaef566bfff433b993a55389
Ryan <watermanrw@gmail.com>
2024-03-28 14:58:41 -0600

    Added support for image generation.

--- src/openai_api.py
+++ src/openai_api.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from typing import List, Dict
 import os
+import webbrowser
+import urllib
 
 load_dotenv()
 
@@ -26,3 +28,23 @@ class GPT(OpenAI):
         for chunk in stream:
             print(chunk.choices[0].delta.content or "", end="")
         print("\n")
+
+class Dalle(OpenAI):
+    """
+    Class to make api calls to the GPT api.
+    """
+    def __init__(self):
+        #super().__init__(api_key=os.environ.get("OPENAI_API_KEY"))
+        self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    
+    def generate_image(self, prompt, size="1024x1024", quality="standard", n=1):
+        response = self.client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size=size,
+            quality=quality,
+            n=1,
+            )
+
+        image_url = response.data[0].url
+        webbrowser.open_new_tab(image_url)
