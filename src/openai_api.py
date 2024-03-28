8214aae11566a57037f42e2801c5c6aae4c4a1f5
Ryan <watermanrw@gmail.com>
2024-03-28 14:45:41 -0600

    Fixed imports and initialization issues

--- src/openai_api.py
+++ src/openai_api.py
@@ -1,23 +1,28 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from typing import List, Dict
 import os
 
 load_dotenv()
-
-client = OpenAI(api_key=os.getenv("API_Key"))
 
 class GPT(OpenAI):
     """
     Class to make api calls to the GPT api.
     """
     def __init__(self):
-        super().__init__()
+        #super().__init__(api_key=os.environ.get("OPENAI_API_KEY"))
+        self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-    def chat(self, messages):
-        stream = client.chat.completions.create(
+    def chat(self, messages: List[Dict[str, str]]):
+        """
+        Messages received as a list of dictionaries and parsed accordingly.
+        """
+        stream = self.client.chat.completions.create(
             model="gpt-4",
             messages=messages,
             stream=True,
         )
+
         for chunk in stream:
             print(chunk.choices[0].delta.content or "", end="")
+        print("\n")
