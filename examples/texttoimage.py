9a1cd0b8725bf383e96ac83f59bd0569fb6e1183
Ryan <watermanrw@gmail.com>
2024-03-30 10:43:16 -0400

    Example to take a user input to Chat GPT and generate an image based on the model response.

---
+++ examples/texttoimage.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('..')
+import src.openai_api
+from pvrecorder import PvRecorder
+
+system = input("Enter the system context, then press enter: ")
+user = input("Enter the user prompt, then press enter: ")
+
+messages = [{"role": "system", "content": f"{system}"},
+            {"role": "user", "content": f"{user}"}]
+
+gpt_client = src.openai_api.GPT()
+
+response = gpt_client.chat(messages)
+
+dalle_client = src.openai_api.Dalle()
+
+image = dalle_client.generate_image(prompt=response)
