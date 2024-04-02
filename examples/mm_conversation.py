bb3ff7a1bea16562fcb4786cad7b269f984daca3
Ryan <watermanrw@gmail.com>
2024-04-01 21:22:21 -0400

    Completed basic example of image generation based on input prompt.

--- examples/mm_conversation.py
+++ examples/mm_conversation.py
@@ -2,14 +2,15 @@ import sys
 sys.path.append('..')
 import src.openai_api as ai
 import os
-from src.utils.keywords import KEYWORDS
+from src.utils.keywords import GENERATE_IMAGE_KEYWORDS, EDIT_IMAGE_KEYWORDS
 
 basepath = os.path.join(os.path.dirname(__file__), "audio")
-filepath = os.path.join(basepath, f'audio_{len(os.listdir(basepath))}.wav')
+filepath = os.path.join(basepath, f'temp.wav')
 
 #Instantiate the GPT, STT, and TTS models
 gpt = ai.GPT()
 stt = ai.STT()
+dalle = ai.Dalle()
 
 #Set up audio capture
 devices = stt.get_devices()
@@ -19,13 +20,39 @@ while True:
     #Record the user input
     stt.record(filepath, index)
     #Transcribe the user input
-    transcription = stt.transcribe()
+    transcript = stt.transcribe()
+
+    print(transcript)
+
+    phrases = []
+    image=False
+    chat=False
+    edit=False
 
     #if a keyword is identified, add it to the text model's list of skills.
-    for phrase in KEYWORDS:
-        if phrase in transcription.lower():
-            print(phrase)
+    for phrase in GENERATE_IMAGE_KEYWORDS:
+        if phrase in transcript.lower():
             gpt.add_skills(phrase)
+            phrases.append(phrase)
+            image=True
 
-    #Format the message then feed the user input to the GPT model
-    response = gpt.stream_to_speech("user", transcription, print_response=True)
+    for phrase in phrases:    
+        if phrase == 'describe':
+            #Format the message then feed the user input to the GPT model
+            response = gpt.stream_to_speech("user", transcript, print_response=True)
+            dalle.generate_image(prompt=response)
+            image=False
+            chat=True
+
+    for phrase in EDIT_IMAGE_KEYWORDS:
+        if phrase in transcript.lower():
+            gpt.add_skills(phrase)
+            dalle.edit_image(transcript, r'./images/temp.png')
+            image=False    
+
+    if image == True:
+        dalle.generate_image(prompt=transcript)
+    if chat == False:
+        response = gpt.stream_to_speech("user", transcript, print_response=True)
+
+
