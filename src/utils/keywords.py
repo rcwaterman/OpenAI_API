3295b89a66d0b4a9dc5fb6c7fdd92dea5c3c3a02
Ryan <watermanrw@gmail.com>
2024-04-01 21:23:27 -0400

    Initial commit of key word list for multi-modal decision making.

---
+++ src/utils/keywords.py
@@ -0,0 +1,42 @@
+"""
+This file contains the list of all text keywords for the API.
+"""
+
+GENERATE_IMAGE_KEYWORDS = frozenset(['generate an image',
+                      'generate me an image',
+                      'create an image',
+                      'create me an image',
+                      'generate a picture',
+                      'generate me a picture',
+                      'create a picture',
+                      'create me a picture',
+                      'show me',
+                      'hello chat gpt',
+                      'paint a picture',
+                      'draw a picture',
+                      'paint an image',
+                      'draw an image',
+                      'paint a painting',
+                      'draw a drawing',
+                      'describe'
+                      ])
+
+EDIT_IMAGE_KEYWORDS = frozenset(['edit the image',
+                      'edit this image',
+                      'edit that image',
+                      'modify the image',
+                      'modify this image',
+                      'modify that image',
+                      'change the image',
+                      'change this image',
+                      'change that image',
+                      'edit the picture',
+                      'edit this picture',
+                      'edit that picture',
+                      'modify the picture',
+                      'modify this picture',
+                      'modify that picture',
+                      'change the picture',
+                      'change this picture',
+                      'change that picture',
+                      'remove the',])
